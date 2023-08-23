7cd533f776e3469fa1487b888900f8e272680e26
Yubinex <janhauck99@gmail.com>
2023-08-23 08:29:06 +0200

    Implemented devise for user accounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: "janhauck99@gmail.com", password: "20771999", password_confirmation: "20771999")
+
+10.times do |x|
+    Post.create(title: "Title #{x}", body: "Body #{x} Words go here", user_id: User.first.id)
+end
