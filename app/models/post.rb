ebf3f0977b533f183298d7342ca428d63a6a7670
Yubinex <janhauck99@gmail.com>
2023-08-25 09:38:32 +0200

    Implemented rich text for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+    has_rich_text :body
     validates :title, presence: true, length: {maximum: 50}
     validates :body, presence: true, length: {maximum: 1500}
     belongs_to :user
